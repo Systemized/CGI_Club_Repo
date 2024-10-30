f47c93f8a60d05837cb1b2b19fcf256b463e270b
Systemized <VincentNguyen.XLV@gmail.com>
2024-10-30 02:11:19 -0400

    Still adding new things.
    Created templates for Home and about pages. also created base.html to reduce redundant html

---
+++ CGI_Club/canvas/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+# Create your views here.
+
+posts = [
+    {
+    "author": "James Games",
+    "title" : "Blog Post",
+    "content":"Hello World",
+    "date_posted":"11/10/2020"
+    }, 
+    {
+    "author": "Corey James",
+    "title" : "Blog Post 2",
+    "content":"Hello Worlds",
+    "date_posted":"11/11/2020"
+    },
+]
+
+def home(request):
+    context = {
+            'posts': posts
+    }
+    return render(request, 'canvas/home.html', context)
+
+
+def about(request):
+    return render(request, 'canvas/about.html')
