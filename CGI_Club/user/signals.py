9a1c26d9e0e5a38ef29ae1ee743585256267137c
Systemized <VincentNguyen.XLV@gmail.com>
2024-11-11 14:32:57 -0500

    used .gitignore to remove .DS_Store from being committed from here onwards. also modified signals to erase old profile pics after new one is added

--- CGI_Club/user/signals.py
+++ CGI_Club/user/signals.py
@@ -9,15 +9,13 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-## ChatGPT with some 'minor' help since im so ass, so idk how bad or good this function is
-# @receiver(pre_save, sender=Profile)
-# def delete_old_profile_pic(sender, instance, **kwargs):
-#     if instance.pk:  # Check if instance is already saved (update, not create)
-#         old_profile = Profile.objects.get(pk=instance.pk)
-#         if old_profile.image != instance.image and old_profile.image.name != 'default.jpg':
-#             old_profile.image.delete(save=False)  # Delete the old file, don't save immediately
-
-
+# ## ChatGPT with some 'minor' help since im so ass, so idk how bad or good this function is
+@receiver(pre_save, sender=Profile)
+def delete_old_profile_pic(sender, instance, **kwargs):
+    if instance.pk:  # Check if instance is already saved (update, not create)
+        old_profile = Profile.objects.get(pk=instance.pk)
+        if old_profile.image != instance.image and old_profile.image.name != 'default.jpg':
+            old_profile.image.delete(save=False)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
